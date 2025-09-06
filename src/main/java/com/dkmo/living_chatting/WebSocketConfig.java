0e4fda506d51a5e508deddfdc889983f2c0c0c16
Danilo <danilokelvemeireles45@gmail.com>
2025-09-06 17:57:22 +0000

    teste

--- src/main/java/com/dkmo/living_chatting/WebSocketConfig.java
+++ src/main/java/com/dkmo/living_chatting/WebSocketConfig.java
@@ -16,7 +16,7 @@ public class WebSocketConfig  implements WebSocketMessageBrokerConfigurer {
         }
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry){
-    registry.addEndpoint("/buildrun-livechat-websocket").setAllowedOrigins("*").withSockJS();
+    registry.addEndpoint("/buildrun-livechat-websocket").setAllowedOrigins("*");
     
     }
 }
