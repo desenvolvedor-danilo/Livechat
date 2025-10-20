34e1afeb3a14fcacff4610495ee600d2b6d9e12d
Danilo Kelve <danilokelvemeireles45@gmail.com>
2025-10-20 11:37:43 -0300

    dockerfile

--- src/main/java/com/dkmo/living_chatting/WebSocketConfig.java
+++ src/main/java/com/dkmo/living_chatting/WebSocketConfig.java
@@ -8,15 +8,17 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig  implements WebSocketMessageBrokerConfigurer {
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry){
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/topics");
         registry.setApplicationDestinationPrefixes("/app");
-        }
+    }
+
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry){
-    registry.addEndpoint("/buildrun-livechat-websocket").setAllowedOrigins("*");
-    
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/buildrun-livechat-websocket").setAllowedOriginPatterns("*")
+                .withSockJS();
+
     }
 }
