4a481b17cd64ef765da5f626e35af917ea0e20a7
Danilo Kelve <danilokelvemeireles45@gmail.com>
2026-06-02 14:06:09 -0300

    Rebuild producao

--- src/main/java/com/dkmo/living_chatting/infrastructure/webmvc/WebMvcConfigure.java
+++ src/main/java/com/dkmo/living_chatting/infrastructure/webmvc/WebMvcConfigure.java
@@ -29,7 +29,8 @@ public class WebMvcConfigure implements WebMvcConfigurer {
 
   @Override
   public void addCorsMappings(CorsRegistry corsRegistry) {
-    corsRegistry.addMapping("/**").allowedOrigins("https://livechat-front-end-73fl.vercel.app")
+    corsRegistry.addMapping("/**")
+        .allowedOrigins("https://livechat-front-end-73fl.vercel.app")
         .allowedMethods("GET", "POST", "PUT", "DELETE").allowedHeaders("*").allowCredentials(true);
   }
 }
