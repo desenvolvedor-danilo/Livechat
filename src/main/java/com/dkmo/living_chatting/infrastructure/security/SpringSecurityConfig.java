d92b16475692c87e5d371c7bfae6c8404ff35477
Danilo Kelve <danilokelvemeireles45@gmail.com>
2026-06-02 14:26:52 -0300

    Rebuild producao

--- src/main/java/com/dkmo/living_chatting/infrastructure/security/SpringSecurityConfig.java
+++ src/main/java/com/dkmo/living_chatting/infrastructure/security/SpringSecurityConfig.java
@@ -18,47 +18,48 @@ import com.dkmo.living_chatting.infrastructure.filter.FilterRequests;
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig {
- @Autowired
- private FilterRequests filter;
-@Bean
-public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationConfiguration
-    configuration, ValidateTokenUseCase validate)throws Exception{
-    return http.csrf(csrf->csrf.disable())
-    .cors(Customizer.withDefaults())
-    .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-    .authorizeHttpRequests(authorize->authorize
-    .requestMatchers(HttpMethod.POST,"/users/login").  permitAll()
-     .requestMatchers(HttpMethod.POST,"/users/create").permitAll() 
-    .requestMatchers("/login", "/login.html"). permitAll()
-    // .requestMatchers(HttpMethod.POST,"/files/save").permitAll()
-    .requestMatchers("/favicon.png").permitAll()
-    .requestMatchers("/list-contacts.css").permitAll()
-    .requestMatchers("/css/**").permitAll()
-    .requestMatchers("/notifications","/notifications.html").permitAll()
-    .requestMatchers(HttpMethod.POST,"/refresh/token").permitAll()
-    .requestMatchers("/main.css").permitAll()
-    .requestMatchers("/uploads/photos-profiles/**","/photos-profiles/**").permitAll()
-.requestMatchers(HttpMethod.GET,"/redirect/page").permitAll()
-    .requestMatchers("/imagens/**").permitAll()
-    .requestMatchers("/cadastro.html").permitAll()
-    .requestMatchers(HttpMethod.POST,"/update/password").permitAll()
+    @Autowired
+    private FilterRequests filter;
 
-    .requestMatchers("/app/chat/private").permitAll()
-    .requestMatchers(HttpMethod.POST,"/refresh/token").permitAll()  
-    .requestMatchers("/private.html").permitAll()
-    .requestMatchers("/configuration.html").permitAll()
-    .requestMatchers("/icons/**").permitAll()
-    .requestMatchers("/buildrun-livechat-websocket/**").permitAll()
-    .requestMatchers("/contacts", "/list-contacts.html").permitAll()
-    .requestMatchers("/app.js").permitAll()
-    .requestMatchers("/js/**").permitAll()
-    .anyRequest().authenticated())
-    .addFilterBefore(filter,UsernamePasswordAuthenticationFilter.class)
-    .exceptionHandling(ex->ex.authenticationEntryPoint((request,response,authException)->{
-            response.setStatus(401);
-        })
-        )
-    .httpBasic(Customizer.withDefaults())
-    .build();
-  } 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationConfiguration configuration,
+            ValidateTokenUseCase validate) throws Exception {
+        return http.csrf(csrf -> csrf.disable())
+                .cors(cors -> {
+                })
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, "/users/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/users/create").permitAll()
+                        .requestMatchers("/login", "/login.html").permitAll()
+                        // .requestMatchers(HttpMethod.POST,"/files/save").permitAll()
+                        .requestMatchers("/favicon.png").permitAll()
+                        .requestMatchers("/list-contacts.css").permitAll()
+                        .requestMatchers("/css/**").permitAll()
+                        .requestMatchers("/notifications", "/notifications.html").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/refresh/token").permitAll()
+                        .requestMatchers("/main.css").permitAll()
+                        .requestMatchers("/uploads/photos-profiles/**", "/photos-profiles/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/redirect/page").permitAll()
+                        .requestMatchers("/imagens/**").permitAll()
+                        .requestMatchers("/cadastro.html").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/update/password").permitAll()
+
+                        .requestMatchers("/app/chat/private").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/refresh/token").permitAll()
+                        .requestMatchers("/private.html").permitAll()
+                        .requestMatchers("/configuration.html").permitAll()
+                        .requestMatchers("/icons/**").permitAll()
+                        .requestMatchers("/buildrun-livechat-websocket/**").permitAll()
+                        .requestMatchers("/contacts", "/list-contacts.html").permitAll()
+                        .requestMatchers("/app.js").permitAll()
+                        .requestMatchers("/js/**").permitAll()
+                        .anyRequest().authenticated())
+                .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling(ex -> ex.authenticationEntryPoint((request, response, authException) -> {
+                    response.setStatus(401);
+                }))
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
 }
